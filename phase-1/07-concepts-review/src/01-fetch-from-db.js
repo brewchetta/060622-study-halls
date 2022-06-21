81695d8710f82350d5d66e1fe80bfb555c68a03f
brewchetta <chett.tiller@gmail.com>
2022-06-21 14:08:19 -0400

    removed unnecessary comment

--- phase-1/07-concepts-review/src/01-fetch-from-db.js
+++ phase-1/07-concepts-review/src/01-fetch-from-db.js
@@ -1,9 +1,6 @@
 // For this first challenge you'll need to fetch the data from the database, parse it, and
 // then console.log it
 
-// Once you've saved the global variable, you should be able to see all your candies
-// in the dev tools console
-
 fetch('http://localhost:4000/candies')
 .then(res => res.json())
 .then(data => console.log('candies: ', candies))
