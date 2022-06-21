737295211076063e4b79fb29e0a1be96948379f0
brewchetta <chett.tiller@gmail.com>
2022-06-21 13:49:17 -0400

    review

---
+++ phase-1/07-concepts-review/src/03-render-item.js
@@ -0,0 +1,8 @@
+// Using the first candy, create a new <div> which shows the candy information as
+// an <h3> for the name, an <img> of its image, and a <p> of its rating
+
+// Add it to the '#candy-container' div
+
+const firstCandy = candies[0]
+
+// If you finish this quickly, do this for all the candies with a `.forEach`
