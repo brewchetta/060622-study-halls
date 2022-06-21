3f43555cee72f51a99a69ff348e80642b1979ce9
brewchetta <chett.tiller@gmail.com>
2022-06-21 14:08:35 -0400

    removed unnecessary text

--- phase-1/07-concepts-review/src/01-fetch-from-db.js
+++ phase-1/07-concepts-review/src/01-fetch-from-db.js
@@ -1,9 +1,2 @@
-// For this first challenge you'll need to fetch the data from the database, parse it, and then
-// add it to the global variable `candies`
-
-let candies = []
-
-// Once you've saved the global variable, you should be able to see all your candies
-// in the dev tools console
-
-console.log('candies: ', candies)
+// For this first challenge you'll need to fetch the data from the database, parse it, and
+// then console.log it
