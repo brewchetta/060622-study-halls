a3c8e3929fbcf55b2e4e6ef16f8d4fc6dafeca94
brewchetta <chett.tiller@gmail.com>
2022-07-21 11:52:09 -0400

    solved with debugging

--- phase-3/02-debugging/totally-messed-up.rb
+++ phase-3/02-debugging/totally-messed-up.rb
@@ -1,23 +1,27 @@
-class brokenStuff
+require 'pry'
 
-  attr_accessor :message :fav_number
+class BrokenStuff
 
-  def initalize(message, fav_number)
+  attr_accessor :message, :fav_number
+
+  def initialize(message, fav_number)
     @message = message
     @fav_number = fav_number
   end
 
-  def message(@message)
-    @message
-  end
+  # def message(@message)
+  #   @message
+  # end
 
-  def message=(new_message)
-    self.message = new_message
-  end
+  # def message=(new_message)
+  #   @message = new_message
+  # end
 
   def print_each_letter
-  @message.each do |letter|
-    puts letter
+    # binding.pry
+    @message.each_char do |letter|
+      puts letter
+    end
   end
 
 end
