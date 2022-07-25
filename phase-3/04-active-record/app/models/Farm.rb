8df394173ed6c8d106899b76e5d04c8173b5b12f
brewchetta <chett.tiller@gmail.com>
2022-07-25 10:04:51 -0400

    gave farms a location

--- phase-3/04-active-record/app/models/Farm.rb
+++ phase-3/04-active-record/app/models/Farm.rb
@@ -1,12 +1,13 @@
 class Farm
 
-  attr_accessor :name, :acreage
+  attr_accessor :name, :location, :acreage
 
   @@all = []
 
-  def initialize(name, acreage)
+  def initialize(name, acreage, location)
     @name = name
     @acreage = acreage
+    @location = location
 
     @@all << self
   end
