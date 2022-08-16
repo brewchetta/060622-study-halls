1b3b5f7b08141d0258ee704c51f8a1f805be4d9e
brewchetta <chett.tiller@gmail.com>
2022-08-16 13:45:42 -0400

    changed seeds so they're valid

--- phase-4/05-putting-it-all-together/db/seeds.rb
+++ phase-4/05-putting-it-all-together/db/seeds.rb
@@ -14,10 +14,10 @@ c3 = Customer.create!(name: "El Chapo", member: false, age: 40)
 puts "Made some customers!"
 
 puts "Making prescriptions..."
-Prescription.create!(name: "Aderall", dosage: 800, legality: true, drugstore: d1, customer: c1)
-Prescription.create!(name: "Antidepressents", dosage: 320, legality: true, drugstore: d2, customer: c2)
-Prescription.create!(name: "Robitussin", dosage: 1000, legality: false, drugstore: d2, customer: c3)
-Prescription.create!(name: "Devil's Lettuce", dosage: 10000, legality: false, drugstore: d1, customer: c2)
+Prescription.create!(name: "Aderall", dosage: 300, legality: true, drugstore: d1, customer: c1)
+Prescription.create!(name: "Antidepressents", dosage: 120, legality: true, drugstore: d2, customer: c2)
+Prescription.create!(name: "Robitussin", dosage: 100, legality: false, drugstore: d2, customer: c3)
+Prescription.create!(name: "Devil's Lettuce", dosage: 400, legality: false, drugstore: d1, customer: c2)
 puts "Made prescriptions!"
 
 puts "Done seeding!"
